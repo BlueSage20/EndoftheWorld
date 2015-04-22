917a387fce80f1309d80ddc0eea84aa6c6fa3560
Nicholas Iebba <nai7804@GDD-46.main.ad.rit.edu>
2015-04-22 17:59:59 -0400

    First Commit
    We imported models of planets, and their textures. But how do we texture
    them?

---
+++ SolarSystem/0_TestBed/Application.cpp
@@ -0,0 +1,37 @@
+#include "ApplicationClass.h"
+void ApplicationClass::InitUserAppVariables()
+{
+	m_pCamera->SetPosition(vector3(0.0f, 0.0f, 15.0f));
+
+	//m_pMeshMngr->LoadModelUnthreaded("Minecraft\\MC_Steve.obj", "Steve");
+	m_pMeshMngr->LoadModelUnthreaded("Planets\\00_Sun.obj", "Sun");
+
+
+	/*for (int nCreepers = 0; nCreepers  < 20; nCreepers ++)
+	{
+		//create 20 instances of one model
+		//sharing memory
+		//sphericalRand give random vector of sphere with specified radius
+		m_pMeshMngr->LoadModelUnthreaded("Minecraft\\MC_Creeper.obj", "Creeper", glm::translate(glm::sphericalRand(5.0f)));
+	}*/
+	
+}
+void ApplicationClass::Update (void)
+{
+	m_pSystem->UpdateTime(); //Update the system
+	m_pMeshMngr->SetModelMatrix(m_m4SelectedObject, m_sSelectedObject); //Setting up the Model Matrix
+	m_pMeshMngr->Update(); //Update the mesh information
+
+	//First person camera movement
+	if(m_bFPC == true)
+		CameraRotation();
+
+	if(m_bArcBall == true)
+	{
+		ArcBall();
+		m_pMeshMngr->SetModelMatrix(m_m4SelectedObject, m_sSelectedObject); //Setting up the Model Matrix
+	}
+
+	printf("FPS: %d\r", m_pSystem->FPS);//print the Frames per Second	
+}
+
